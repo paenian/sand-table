1fc4680a8ca19bcf72d0239b356938cbf6762dda
Paul Chase <paenian@gmail.com>
2017-10-16 21:27:07 -0400

    Added a quick python gcode sender.
    The coordinate transform doesn't work yet :-)

--- software/sand_trace/sand_trace.py
+++ software/sand_trace/sand_trace.py
@@ -1,78 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from tkinter import *
-__author__ = 'Chuntao Lu'
+import serial
+import time
+import random
+import math
+
+__author__ = 'Paul Chase'
 
 
-class Paint(object):
-
-    DEFAULT_PEN_SIZE = 5.0
-    DEFAULT_COLOR = 'black'
+class Sand_Trace(object):
+    MOTOR_DIST = 417
 
     def __init__(self):
-        self.root = Tk()
+        self.serial = serial.Serial('/dev/ttyACM1',115200)
+        
+        # Wake up grbl
+        self.serial.write("\r\n\r\n")
+        time.sleep(2)   # Wait for grbl to initialize 
+        self.serial.flushInput()  # Flush startup text in serial input
 
-        self.pen_button = Button(self.root, text='pen', command=self.use_pen)
-        self.pen_button.grid(row=0, column=0)
+        # Stream g-code to grbl
+        for x in range(0,10):
+            self.send_move( 50, 50)
+            self.send_move( 50,-50)
+            self.send_move(-50,-50)
+            self.send_move(-50, 50)
 
-        self.brush_button = Button(self.root, text='brush', command=self.use_brush)
-        self.brush_button.grid(row=0, column=1)
+        self.serial.close()
 
-        self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
-        self.eraser_button.grid(row=0, column=3)
 
-        self.choose_size_button = Scale(self.root, from_=1, to=10, orient=HORIZONTAL)
-        self.choose_size_button.grid(row=0, column=4)
+    def send_move(self, x, y):
+        dist = self.MOTOR_DIST
+        print('Converting: ( ' + str(x) +  " , " + str(y) + ' )')
+        
+        
+        
+        a = math.sqrt(x*x+y*y)
+        b = math.sqrt(x*x+(dist-y)*(dist-y))
 
-        self.c = Canvas(self.root, bg='white', width=600, height=600)
-        self.c.grid(row=1, columnspan=5)
+        move = "G0 X"+str(a)+" Y"+str(b)
 
-        self.setup()
-        self.root.mainloop()
-
-    def setup(self):
-        self.old_x = None
-        self.old_y = None
-        self.line_width = self.choose_size_button.get()
-        self.color = self.DEFAULT_COLOR
-        self.eraser_on = False
-        self.active_button = self.pen_button
-        self.c.bind('<B1-Motion>', self.paint)
-        self.c.bind('<ButtonRelease-1>', self.reset)
-
-    def use_pen(self):
-        self.activate_button(self.pen_button)
-
-    def use_brush(self):
-        self.activate_button(self.brush_button)
-
-    def use_eraser(self):
-        self.activate_button(self.eraser_button, eraser_mode=True)
-
-    #TODO: reset canvas
-    #TODO: undo and redo
-    #TODO: draw triangle, rectangle, oval, text
-
-    def activate_button(self, some_button, eraser_mode=False):
-        self.active_button.config(relief=RAISED)
-        some_button.config(relief=SUNKEN)
-        self.active_button = some_button
-        self.eraser_on = eraser_mode
-
-    def paint(self, event):
-        self.line_width = self.choose_size_button.get()
-        paint_color = 'white' if self.eraser_on else self.color
-        if self.old_x and self.old_y:
-            self.c.create_line(self.old_x, self.old_y, event.x, event.y,
-                               width=self.line_width, fill=paint_color,
-                               capstyle=ROUND, smooth=TRUE, splinesteps=36)
-        self.old_x = event.x
-        self.old_y = event.y
-
-    def reset(self, event):
-        self.old_x, self.old_y = None, None
-
+        print('Sending: ' + move)
+        self.serial.write(move + '\n')
+        self.grbl_out = self.serial.readline()
+        print(' : ' + move.strip())
 
 if __name__ == '__main__':
-    ge = Paint()
+    app = Sand_Trace()
