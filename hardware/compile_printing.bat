59651d513089f2d7853a600831ffb2c40dfc87f9
Paul Chase <paenian@gmail.com>
2018-03-05 17:23:08 -0500

    Updated crossbot STLs, added the base beam mount.
    Right now the beam mount will restrict the beam a fair bit, but gotta get something going - worried about table stability.

--- hardware/compile_printing.bat
+++ hardware/compile_printing.bat
@@ -8,3 +8,4 @@ openscad -o crossbot_stls/double_idler.stl -D part=1 crossbot.scad &
 openscad -o crossbot_stls/beam_carriage.stl -D part=2 crossbot.scad &
 openscad -o crossbot_stls/motor_mount.stl -D part=3 crossbot.scad &
 openscad -o crossbot_stls/offset_motor_mount.stl -D part=4 crossbot.scad &
+openscad -o crossbot_stls/base_beam_mount.stl -D part=5 crossbot.scad &
