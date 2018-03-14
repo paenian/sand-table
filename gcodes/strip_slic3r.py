d082642fe46aa47b7bd630fd2c3a3aa51e136777
Paul J Chase <paenian@gmail.com>
2018-03-14 00:41:36 -0400

    Added newest grbl, gcode creation bits
    She works!  Gcode is a bit crusty, but it can be lived with for the
    faire.

---
+++ gcodes/strip_slic3r.py
@@ -0,0 +1,18 @@
+import fileinput
+import re
+
+# regexes for deleting the whole line
+del_line = ['M.*']
+
+# regexes for simply removing the match
+del_match = ['A[0-9.-]+']
+
+for line in fileinput.input(inplace=True, backup='.bak'):
+	for entry in del_line:
+		p = re.compile(entry)
+		if p.match(line):
+			line = ''
+	for entry in del_match:
+		p = re.compile(entry)
+		line = p.sub('', line)
+	print(line, end='', flush=True)
